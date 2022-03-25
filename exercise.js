7babc07f1fec3785a8276d5f2d4c50ecacbd9033
aconkli9 <aconkli9@ford.com>
2022-03-25 12:48:39 -0400

    Complete excercises

--- exercise.js
+++ exercise.js
@@ -1,3 +1,30 @@
+
+function getTotalItemSales (sales, Id) {
+    return sales.filter(sale => sale.itemId === Id)
+    .map(sortedSale => sortedSale.quantity)
+    .reduce((sum, number) => {
+        return sum + number
+    }, 0)
+}
+
+function addTotalSalesToItems (sales, itemList) {
+    return itemList.map(item => {
+        return {
+            ...item, 
+            quantity: getTotalItemSales (sales, item.id)
+        }
+    })
+}
+
+function addTotalValueToItems (sales, itemList) {
+    return addTotalSalesToItems (sales, itemList).map(item => {
+        return {
+            ...item,
+            totalValue: (item.price * item.quantity).toFixed(2)
+        }
+    })
+}
+
 module.exports = {
     // Uncomment these functions as you write them
     getTotalItemSales,
